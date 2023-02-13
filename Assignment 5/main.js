f378b31acf301141c9658fa5b14c5a22314125cc
Rehmat Gul <120668409+Romanstar0213@users.noreply.github.com>
2023-02-13 06:44:50 -0800

    Add files via upload

---
+++ Assignment 5/main.js
@@ -0,0 +1,107 @@
+// CHAPTER 14-16
+
+        // Question no 1
+        
+        // arr = []
+
+        // Question no 2
+
+        // obj = {}
+
+        // Question no 3
+        
+        // arr = ["String"]
+        // alert(arr)
+
+        // Question no 4
+
+
+        // Question no 5
+        
+        // arr = [10]
+        // alert(arr)
+        
+        // Question no 6
+        
+        // arr = [true]
+        // alert(arr[0])
+
+        // Question no 7
+
+        // var edu = ["FSC", "HSC", "BSC","BS", "BCOM", "MS", "M-Phil", "PhD"]
+        // document.write(edu[0]+"<br>"+edu[1]+"<br>"+edu[2]+"<br>"+edu[3]+"<br>"+edu[4]+"<br>"+edu[5]+"<br>"+edu[6]+"<br>"+edu[7])
+
+        // Question no 8
+
+        // var stName = ["Michel","John","Tony"]
+        // var stMarks = [320,230,480]
+        // var stTMarks = [500]
+        // document.write("Score of "+ stName[0]+" is "+stMarks[0]+" Percentage: "+ (stMarks[0]/stTMarks*100)+"<br>"+"Score of "+ stName[1]+" is "+stMarks[1]+" Percentage: "+ (stMarks[1]/stTMarks*100)tName[2]+"+"<br>"+"Score of "+ s is "+stMarks[2]+" Percentage: "+ (stMarks[2]/stTMarks*100))
+
+        // Question no 9
+
+        // var colors = ["red","green","blue"]
+
+        // var  inp = prompt("Which color you want to add")
+        // colors.unshift(inp)
+
+        // var  inp2 = prompt("Which color you want to add")
+        // colors.push(inp2)
+
+        // var  inp3 = prompt("Which another color you want to add")
+        // colors.unshift(inp3)
+        
+        // colors.shift()
+
+        // colors.pop()
+
+        // var  inp4 = prompt("Which color you want to add")
+        // var index = +prompt("Index number")
+        // colors.splice(index,0,inp4)
+
+        // var index = +prompt("Index number")
+        // colors.splice(index,1)
+        
+        // alert(colors)
+
+        // Question no 10
+
+        // var inp1 = +prompt("Enter score 1")
+        // var inp2 = +prompt("Enter score 2")
+        // var inp3 = +prompt("Enter score 3")
+        // var score = [inp1,inp2,inp3]
+        // score.sort()
+        // alert(score)
+
+        // Question no 11
+
+        // var cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+        // city = cities.slice(2,4)
+        // document.write("Cities List"+"<br>"+cities+"<br>"+"Selected Cities"+"<br>"+city)
+
+        // Question no 12
+        
+        // var arr = ["This","is"," my" ,"cat"]
+        //  var ar = arr.join(" ")
+        // alert(ar)
+        
+        // Question no 13
+        
+        // var a = ["keyboard","mouse ","printer","monitor"]
+        // a.shift()
+        // inp = prompt("Enter accessories")
+        // a.unshift(inp)
+        // alert(a)
+        
+        // Question no 14
+
+        // var a = ["keyboard","mouse ","printer","monitor"]
+        // a.shift()
+        // inp = prompt("Enter accessories")
+        // a.push(inp)
+        // alert(a)
+        
+        // Question no 15
+
+        // var arr = ["Apple","Nokia","Motorola","Samsung","Sony"]
+        // document.write("<select>"+"<option>"+ arr[0]+"</option"+"<br>"+"<option>"+ arr[1]+"</option"+"</select>"+"<br>"+"<option>"+ arr[2]+"</option"+"</select>"+"<br>"+"<option>"+ arr[3]+"</option"+"</select>"+"<br>"+"<option>"+ arr[4]+"</option"+"</select>")
